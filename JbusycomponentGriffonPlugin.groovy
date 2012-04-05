bcb92db7dc7663d4a1b08b9346d6fe3192bfd5f7
saschaklein <info@aklein.org>
2012-04-05 14:57:42 +0200

    Upgrade to Griffon 0.9.5

--- JbusycomponentGriffonPlugin.groovy
+++ JbusycomponentGriffonPlugin.groovy
@@ -1,37 +1,36 @@
-/*
- * Copyright 2009-2010 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the 'License');
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an 'AS IS' BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/**
- * @author Andres Almiray
- */
 class JbusycomponentGriffonPlugin {
-    def version = '0.5.2'
-    def dependsOn = ['swingx-builder': '0.5', jxlayer: 0.2]
-    def toolkits = ['swing']
-    def griffonVersion = '0.9.3 > *'
-    def license = 'Apache Software License 2.0'
+    // the plugin version
+    String version = '0.5.3'
+    // the version or versions of Griffon the plugin is designed for
+    String griffonVersion = '0.9.5 > *'
+    // the other plugins this plugin depends on
+    Map dependsOn = ['swingx-builder': '0.6', jxlayer: '0.2']
+    // resources that are included in plugin packaging
+    List pluginIncludes = []
+    // the plugin license
+    String license = 'Apache Software License 2.0'
+    // Toolkit compatibility. No value means compatible with all
+    // Valid values are: swing, javafx, swt, pivot, qt
+    List toolkits = ['swing']
+    // Platform compatibility. No value means compatible with all
+    // Valid values are:
+    // linux, linux64, windows, windows64, macosx, macosx64, solaris
+    List platforms = []
+    // URL where documentation can be found
+    String documentation = ''
+    // URL where source can be found
+    String source = 'https://github.com/griffon/griffon-jbusycomponent-plugin'
 
-    def author = 'Andres Almiray'
-    def authorEmail = 'aalmiray@users.sourceforge.net'
-    def title = 'Enhance any swing components with a \'busy\' state'
-    def description = '''
+    List authors = [
+        [
+            name: 'Andres Almiray',
+            email: 'aalmiray@yahoo.com'
+        ]
+    ]
+    String title = 'Enhance any swing components with a \'busy\' state'
+    // accepts Markdown syntax. See http://daringfireball.net/projects/markdown/ for details
+    String description = '''
 JBusyComponent: Enhance any swing components with a 'busy' state.
 http://code.google.com/p/jbusycomponent
 '''
-
-    // URL to the plugin's documentation
-    def documentation = 'http://griffon.codehaus.org/JBusyComponent+Plugin'
 }
